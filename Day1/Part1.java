a62d9e2daccc2a848fa77becabb5a52c6ebd38ee
bng40@stuy.edu <baronng25@gmail.com>
2022-12-01 16:59:21 -0500

    made broken day1(need a way to detect empty lines)

--- Day1/Part1.java
+++ Day1/Part1.java
@@ -1,2 +1,39 @@
 import java.util.*;
 import java.io.*;
+
+public class Part1{
+  public static void bubblesort(ArrayList<Integer> arr){
+    int temp;
+    int leaf = 1;
+    while (leaf != 0){
+      leaf = 0;
+      for (int i = 1; i < arr.size(); i++){
+        if (arr.get(i-1) > arr.get(i)){
+          temp = arr.get(i);
+          arr.set(i, arr.get(i-1));
+          arr.set(i-1, temp);
+          leaf = 1;
+        }
+      }
+    }
+  }
+
+  public static void main (String[] args){
+    Scanner sc = new Scanner (System.in);
+    ArrayList <Integer> sums = new ArrayList <Integer> ();
+    while (sc.hasNextLine()){
+      ArrayList <Integer> sum = new ArrayList <Integer> ();
+      int total = 0;
+      while (try {(!(sc.nextLine().equals("")))} catch (NoSuchElementException e){true}){
+        sum.add (sc.nextInt());
+      }
+      for (int i = 0; i < sum.size(); i++){
+        total += sum.get(i);
+      }
+      sums.add(total);
+    }
+    bubblesort (sums);
+    int x = sums.get(sums.size()-1);
+    System.out.println (x);
+  }
+}
