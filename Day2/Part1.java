f37b750877a04a060524120b9fcd2040ca968fb3
bng40@stuy.edu <baronng25@gmail.com>
2022-12-02 14:48:51 -0500

    started day2

---
+++ Day2/Part1.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class Part1{
+  public static int winner (String you, String him){
+    if (you.equals(him)){
+      return 3;
+    }
+    if (you.equals("A") && him.equals ("B")){
+      return 0;
+    }
+    if (you.equals("A") && him.equals ("C")){
+      return 6;
+    }
+    if (you.equals("B") && him.equals ("A")){
+      return 6;
+    }
+    if (you.equals("B") && him.equals ("C")){
+      return 0;
+    }
+    if (you.equals("C") && him.equals ("A")){
+      return 0;
+    }
+    return 6;
+  }
+
+  public static void main (String[] args){
+    Scanner sc = new Scanner (System.in);
+    HashMap <String,Integer> key = new HashMap <>();
+    key.put ("A", 1);
+    key.put ("B", 2);
+    key.put ("C", 3);
+    ArrayList <String[]> bruh = new ArrayList <String[]> ();
+    int score = 0;
+    while (sc.hasNextLine()){
+      String s = sc.nextLine();
+      bruh.add (s.split(" "));
+    }
+    for (int i = 0; i < bruh.size(); i++){
+      String x = (bruh.get(i))[0];
+      String y = (bruh.get(i))[1];
+      if (y.equals ("X")){
+        y = "A";
+      }
+      if (y.equals ("Y")){
+        y = "B";
+      }
+      if (y.equals ("Z")){
+        y = "C";
+      }
+      score += key.get(x) + winner (x,y);
+    }
+    System.out.println(score);
+  }
+}
