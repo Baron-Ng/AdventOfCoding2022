85d4ff7684f65f08eb0e59f5ce3b78911202f841
Barppy <103770975+Barppy@users.noreply.github.com>
2022-12-08 15:31:51 -0500

    Update Part1.java

--- Day8/Part1.java
+++ Day8/Part1.java
@@ -3,12 +3,12 @@ import java.io.*;
 
 public class Part1{
 	public static ArrayList <ArrayList <Integer>> input (Scanner sc){
-		ArrayList <Integer> trees = new ArrayList<>();
+		ArrayList <ArrayList <Integer>> trees = new ArrayList<>();
 		while (sc.hasNextLine()){
 			ArrayList <Integer> broh = new ArrayList<>();
 			String s = sc.nextLine();
 			String [] bruh = s.split("");
-			for (int i = 0; i < bruh.length(); i++){
+			for (int i = 0; i < bruh.length; i++){
 				broh.add (Integer.parseInt(bruh[i]));
 			}
 			trees.add (broh);
@@ -19,22 +19,30 @@ public class Part1{
 	public static ArrayList <ArrayList <Boolean>> arrayTrees (ArrayList <ArrayList <Integer>> trees){
 		ArrayList <ArrayList <Boolean>> visible = new ArrayList <>();
 		for (int i = 0; i < trees.size(); i++){
-			ArrayList <Integer> broh = new ArrayList<>();
-			for (int j = 0; j < trees.get(i).size()){
+			ArrayList <Boolean> broh = new ArrayList<>();
+			for (int j = 0; j < trees.get(i).size(); j++){
 				int height = trees.get(i).get(j);
-				boolean b = true;
+				boolean b = false;
+				boolean temp = true;
 				for (int k = j - 1; k > 0; k--){ // moving left on x axis
-					b = b && (height > trees.get(i).get(k));
+					temp = temp && (height > trees.get(i).get(k));
 				}
+				b = b || temp;
+				temp = true;
 				for (int k = j + 1; k < trees.get(i).size(); k++){  // moving right on x axis
-					b = b && (height > trees.get(i).get(k));
+					temp = temp && (height > trees.get(i).get(k));
 				}
+				b = b || temp;
+				temp = true;
 				for (int k = i - 1; k > 0; k--){  // moving up on y axis
-					b = b && (height > trees.get(k).get(j));
+					temp = temp && (height > trees.get(k).get(j));
 				}
+				b = b || temp;
+				temp = true;
 				for (int k = i + 1; k < trees.size(); k++){  // moving down on y axis
-					b = b && (height > trees.get(k).get(j));
+					temp = temp && (height > trees.get(k).get(j));
 				}
+				b = b || temp;
 				broh.add (b);
 			}
 			visible.add (broh);
@@ -44,8 +52,17 @@ public class Part1{
 
 	public static void main (String [] args){
 		Scanner sc = new Scanner (System.in);
+		int sum = 0;
 		ArrayList <ArrayList<Integer>> trees = input(sc);
+		System.out.println (trees);
 		ArrayList <ArrayList<Boolean>> bools = arrayTrees (trees);
-		
+		for (int i = 0; i < bools.size(); i++){
+			for (int j = 0; j < bools.get(i).size(); j++){
+				if (bools.get(i).get(j)){
+					sum++;
+				}
+			}
+		}
+		System.out.println(sum);
 	}
 }
