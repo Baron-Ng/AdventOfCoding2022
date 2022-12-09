60d8793fecd8062ba352361d2c472dcdc59063fe
Barppy <103770975+Barppy@users.noreply.github.com>
2022-12-09 10:59:43 -0500

    start of part 1

---
+++ Day9/Part1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Part1{
+	public static void check (Point h, Point t){
+		int hx = h.getX();
+		int hy = h.getY();
+		int tx = t.getX();
+		int ty = t.getY();
+
+
+	}
+}
