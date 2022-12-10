085a2dfce096f5ad1427f039156266184d74a38e
unknown <baronng25@gmail.com>
2022-12-10 14:46:28 -0500

    day 9 was infact not free (done, but took 8 hours so who really won in the end)

--- Day9/Cord2.java
+++ Day9/Cord2.java
@@ -51,74 +51,144 @@ public class Cord2{
 		x = otherX;
 		y = otherY;
 	}
-	public void left (Cord2 head, int move){
-		for (int i = 0; i < move; i++){
-			save(head);
-			head.setX (head.getX() - 1);
-			boolean b = distance(head);
-			if (!b){
-				move();
-			}
-		}
-	}
 
-	public void right (Cord2 head, int move){
-		for (int i = 0; i < move; i++){
-			save(head);
-			head.setX (head.getX() + 1);
-			boolean b = distance(head);
-			if (!b){
-				move();
-			}
-		}
-	}
-
-	public void up (Cord2 head, int move){
-		for (int i = 0; i < move; i++){
-			save(head);
-			head.setY (head.getY() + 1);
-			boolean b = distance(head);
-			if (!b){
-				move();
-			}
-		}
-	}
-
-	public void down (Cord2 head, int move){
-		for (int i = 0; i < move; i++){
-			save(head);
-			head.setY (head.getY() - 1);
-			boolean b = distance(head);
-			if (!b){
-				move();
-			}
-		}
-	}
-
-	public void choose (Cord2 head, int move, char direction){
+	public void direction (char direction){
 		if (direction == 'L'){
-			left(head, move);
+			setX (getX() - 1);
 		}
 		if (direction == 'R'){
-			right(head, move);
+			setX (getX() + 1);
 		}
 		if (direction == 'D'){
-			down(head, move);
+			setY (getY() - 1);
 		}
 		if (direction == 'U'){
-			up(head, move);
+			setY (getY() + 1);
 		}
 	}
 
-	public void tailToTail (Cord2 head,Cord2 tail2,Cord2 tail3,Cord2 tail4,Cord2 tail5,Cord2 tail6,Cord2 tail7,Cord2 tail8,Cord2 tail9){
-		for (int i = 0; i < move; i++){
-			save(head);
-			head.setY (head.getY() - 1);
-			boolean b = distance(head);
-			if (!b){
-				move();
-			}
+	public void tailFollow (Cord2 head){
+		int headX = head.getX();
+		int headY = head.getY();
+		int diffX = headX - x;
+		int diffY = headY - y;
+		if (diffX == -2 && diffY == -1){ // left 3 // bottom
+			setX (getX() - 1);
+			setY (getY() - 1);
 		}
+		if (diffX == -2 && diffY == 0){ // middle
+			setX (getX() - 1);
+		}
+		if (diffX == -2 && diffY == 1){ // top
+			setX (getX() - 1);
+			setY (getY() + 1);
+		}
+		if (diffX == 2 && diffY == -1){ // right 3, bottom
+			setX (getX() + 1);
+			setY (getY() - 1);
+		}
+		if (diffX == 2 && diffY == 0){// middle
+			setX (getX() + 1);
+		}
+		if (diffX == 2 && diffY == 1){ // top
+			setX (getX() + 1);
+			setY (getY() + 1);
+		}
+		if (diffX == -1 && diffY == 2){ // top 3, left
+			setX (getX() - 1);
+			setY (getY() + 1);
+		}
+		if (diffX == 0 && diffY == 2){// middle
+			setY (getY() + 1);
+		}
+		if (diffX == 1 && diffY == 2){ // right
+			setX (getX() + 1);
+			setY (getY() + 1);
+		}
+		if (diffX == -1 && diffY == -2){ // bottom 3 // left
+			setX (getX() - 1);
+			setY (getY() - 1);
+		}
+		if (diffX == 0 && diffY == -2){ // middle
+			setY (getY() - 1);
+		}
+		if (diffX == 1 && diffY == -2){ // right
+			setX (getX() + 1);
+			setY (getY() - 1);
+		}
+		if (diffX == -2 && diffY == 2){ //diagonals  // top left
+			setX (getX() - 1);
+			setY (getY() + 1);
+		}
+		if (diffX == 2 && diffY == 2){ // top right
+			setX (getX() + 1);
+			setY (getY() + 1);
+		}
+		if (diffX == 2 && diffY == -2){ // bottom right
+			setX (getX() + 1);
+			setY (getY() - 1);
+		}
+		if (diffX == -2 && diffY == -2){ // bottom left
+			setX (getX() - 1);
+			setY (getY() - 1);
+		}
+	}
+
+	public static ArrayList <ArrayList <Integer>> headToTail (int move, char direction, Cord2 head, Cord2 tail1, Cord2 tail2,Cord2 tail3,Cord2 tail4,Cord2 tail5,Cord2 tail6,Cord2 tail7,Cord2 tail8,Cord2 tail9){
+		ArrayList <ArrayList <Integer>> nice = new ArrayList<>();
+		for (int i = 0; i < move; i++){
+			ArrayList <Integer> bruh = new ArrayList<>();
+			tail1.save(head);
+			tail2.save(tail1);
+			tail3.save(tail2);
+			tail4.save(tail3);
+			tail5.save(tail4);
+			tail6.save(tail5);
+			tail7.save(tail6);
+			tail8.save(tail7);
+			tail9.save(tail8);
+			head.direction(direction);
+			boolean b = tail1.distance(head);
+			if (!b){
+				tail1.tailFollow(head);
+				b = tail2.distance(tail1);
+				if (!b){
+					tail2.tailFollow(tail1);
+					b = tail3.distance(tail2);
+					if (!b){
+						tail3.tailFollow(tail2);
+						b = tail4.distance(tail3);
+						if (!b){
+							tail4.tailFollow(tail3);
+							b = tail5.distance(tail4);
+							if (!b){
+								tail5.tailFollow(tail4);
+								b = tail6.distance(tail5);
+								if (!b){
+									tail6.tailFollow(tail5);
+									b = tail7.distance(tail6);
+									if (!b){
+										tail7.tailFollow(tail6);
+										b = tail8.distance(tail7);
+										if (!b){
+											tail8.tailFollow(tail7);
+											b = tail9.distance(tail8);
+											if (!b){
+												tail9.tailFollow(tail8);
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			bruh.add (tail9.getX());
+			bruh.add (tail9.getY());
+			nice.add (bruh);
+		}
+		return nice;
 	}
 
 	public static ArrayList <ArrayList <Integer>> removeDupes (ArrayList <ArrayList <Integer>> nice){
@@ -134,12 +204,11 @@ public class Cord2{
 		}
 		return nice;
 	}
-
-	public 
-
-	public static void main (String[] args){
+	
+	public static void main (String [] args){
 		Scanner sc = new Scanner (System.in);
-		ArrayList <ArrayList <Integer>> nice = new ArrayList<>();
+		ArrayList <ArrayList <ArrayList<Integer>>> nice = new ArrayList<>();
+		ArrayList <ArrayList<Integer>> cords = new ArrayList<>();
 		ArrayList <Character> d = new ArrayList<>();
 		ArrayList <Integer> m = new ArrayList<>();
 		Cord2 head = new Cord2();
@@ -159,11 +228,17 @@ public class Cord2{
 		for (int k = 0; k < d.size(); k++){
 			char direction = d.get(k);
 			int move = m.get(k);
-			ArrayList <Integer> bruh = new ArrayList<>();
-			tail1.choose (head, move, direction);
-			
+			ArrayList <ArrayList<Integer>> bruh = new ArrayList<>();
+			bruh = headToTail(move, direction, head, tail1, tail2, tail3, tail4, tail5, tail6, tail7, tail8, tail9);
+			nice.add (bruh);
 		}
-		System.out.println (nice.size());
+		for (int i = 0; i < nice.size(); i++){
+			for (int j = 0; j < nice.get(i).size(); j++){
+				cords.add (nice.get(i).get(j));
+			}
+		}
+		cords = removeDupes(cords);
+		System.out.println (cords.size());
 		sc.close();
 	}
 }
