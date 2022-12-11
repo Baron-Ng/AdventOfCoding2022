e247944f445e5210eefc1c97e41e29af8ba1c876
unknown <baronng25@gmail.com>
2022-12-11 18:46:26 -0500

    day 11 idk how to optimize part 2 so big L for me

--- Day11/Monk2.java
+++ Day11/Monk2.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
-public class Monk {
-    private ArrayList <Integer> items;
+public class Monk2 {
+    private ArrayList <Long> items;
     private int div;
 
-    public Monk (ArrayList <Integer> items, int div){
+    public Monk2 (ArrayList <Long> items, int div){
         this.items = items;
         this.div = div;
     }
@@ -13,11 +13,11 @@ public class Monk {
         return (items.get(index) % div == 0);
     }
 
-    public int get (int i){
+    public Long get (int i){
         return items.get(i);
     }
 
-    public void add (int item){
+    public void add (Long item){
         items.add (item);
     }
 
@@ -25,51 +25,55 @@ public class Monk {
         return items.size();
     }
 
-    public void operation (int index, int item){
+    public void printList (){
+        System.out.println (items);
+    }
+
+    public void operation (int index, Long item){
         items.set (index, item);
     }
 
-    public void change (Monk other, int index){
-        other.add (items.get(index));
+    public void change (Monk2 other, int index){
+        Long add = items.get(index);
+        other.add (add);
         items.remove (index);
     }
 
-    public static ArrayList <Integer> toArrayList (int [] list){
-        ArrayList <Integer> arr = new ArrayList<>();
+    public static ArrayList <Long> toArrayList (int [] list){
+        ArrayList <Long> arr = new ArrayList<>();
         for (int i = 0; i < list.length; i++){
-            arr.add (list[i]);
+            arr.add ((long)list[i]);
         }
         return arr;
     }
     public static void main (String [] args){
         int [] list1 = {71, 86};
-        Monk one = new Monk (toArrayList(list1), 19);
+        Monk2 one = new Monk2 (toArrayList(list1), 19);
         int [] list2 = {66, 50, 90, 53, 88, 85};
-        Monk two = new Monk (toArrayList(list2), 2);
+        Monk2 two = new Monk2 (toArrayList(list2), 2);
         int [] list3 = {97, 54, 89, 62, 84, 80, 63};
-        Monk three = new Monk (toArrayList(list3), 13);
+        Monk2 three = new Monk2 (toArrayList(list3), 13);
         int [] list4 = {82, 97, 56, 92};
-        Monk four = new Monk (toArrayList(list4), 5);
+        Monk2 four = new Monk2 (toArrayList(list4), 5);
         int [] list5 = {50, 99, 67, 61, 86};
-        Monk five = new Monk (toArrayList(list5), 7);
+        Monk2 five = new Monk2 (toArrayList(list5), 7);
         int [] list6 = {61, 66, 72, 55, 64, 53, 72, 63};
-        Monk six = new Monk (toArrayList(list6), 11);
+        Monk2 six = new Monk2 (toArrayList(list6), 11);
         int [] list7 = {59, 79, 63};
-        Monk seven = new Monk (toArrayList(list7), 17);
+        Monk2 seven = new Monk2 (toArrayList(list7), 17);
         int [] list8 = {55};
-        Monk eight = new Monk (toArrayList(list8), 3);
-        int score1 = 0;
-        int score2 = 0;
-        int score3 = 0;
-        int score4 = 0;
-        int score5 = 0;
-        int score6 = 0;
-        int score7 = 0;
-        int score8 = 0;
-        for (int rounds = 0; rounds < 20; rounds++){
+        Monk2 eight = new Monk2 (toArrayList(list8), 3);
+        long score1 = 0;
+        long score2 = 0;
+        long score3 = 0;
+        long score4 = 0;
+        long score5 = 0;
+        long score6 = 0;
+        long score7 = 0;
+        long score8 = 0;
+        for (long rounds = 0; rounds < 10000; rounds++){
             for (int i = 0; i < one.size(); i++){
                 one.operation(i, one.get(i) * 13);
-                one.operation(i, (one.get(i) / 3));
                 boolean b = one.test(i);
                 if (b){
                     one.change(seven, i);
@@ -82,7 +86,6 @@ public class Monk {
             }
             for (int i = 0; i < two.size(); i++){
                 two.operation(i, two.get(i) + 3);
-                two.operation(i, (two.get(i) / 3));
                 boolean b = two.test(i);
                 if (b){
                     two.change(six, i);
@@ -95,7 +98,6 @@ public class Monk {
             }
             for (int i = 0; i < three.size(); i++){
                 three.operation(i, three.get(i) + 6);
-                three.operation(i, (three.get(i) / 3));
                 boolean b = three.test(i);
                 if (b){
                     three.change(five, i);
@@ -108,7 +110,6 @@ public class Monk {
             }
             for (int i = 0; i < four.size(); i++){
                 four.operation(i, four.get(i) + 2);
-                four.operation(i, (four.get(i) / 3));
                 boolean b = four.test(i);
                 if (b){
                     four.change(seven, i);
@@ -121,7 +122,6 @@ public class Monk {
             }
             for (int i = 0; i < five.size(); i++){
                 five.operation(i, five.get(i) * five.get(i));
-                five.operation(i, (five.get(i) / 3));
                 boolean b = five.test(i);
                 if (b){
                     five.change(six, i);
@@ -134,7 +134,6 @@ public class Monk {
             }
             for (int i = 0; i < six.size(); i++){
                 six.operation(i, six.get(i) + 4);
-                six.operation(i, (six.get(i) / 3));
                 boolean b = six.test(i);
                 if (b){
                     six.change(four, i);
@@ -147,7 +146,6 @@ public class Monk {
             }
             for (int i = 0; i < seven.size(); i++){
                 seven.operation(i, seven.get(i) * 7);
-                seven.operation(i, (seven.get(i) / 3));
                 boolean b = seven.test(i);
                 if (b){
                     seven.change(three, i);
@@ -160,7 +158,6 @@ public class Monk {
             }
             for (int i = 0; i < eight.size(); i++){
                 eight.operation(i, eight.get(i) + 7);
-                eight.operation(i, (eight.get(i) / 3));
                 boolean b = eight.test(i);
                 if (b){
                     eight.change(three, i);
@@ -172,11 +169,9 @@ public class Monk {
                 score8++;
             }
         }
-        int [] sort = {score1,score2,score3,score4,score5,score6,score7,score8};
+        long [] sort = {score1,score2,score3,score4,score5,score6,score7,score8};
         Arrays.sort (sort);
-        for (int i = 0; i < sort.length; i++){
-            System.out.println (sort[i]);
-        }
-        System.out.println (sort[6] * sort[7]);
+        System.out.println (sort[6] + " " + sort[7]);
+        System.out.println ((long)sort[6] * (long)sort[7]);
     }
 }
